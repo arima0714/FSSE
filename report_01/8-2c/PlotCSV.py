4ffe3d338634c2d98b5e488f76aa0cd0a0da9bcc
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-12-17 16:12:12 +0900

    n(t)の合計を保持するためのリストを追加した

--- report_01/8-2c/PlotCSV.py
+++ report_01/8-2c/PlotCSV.py
@@ -13,4 +13,13 @@ col_x = Output["t"]
 col_y = Output["n(t)"]
 
 plt.plot(col_x, col_y)
+plt.xlabel("t")
+plt.ylabel("n(t)")
 plt.savefig("./PlotCSV.png")
+
+
+time_average = []
+sum_t = 0
+for num in col_y:
+    sum_t == num
+    time_average.append(sum_t)
