bced023fd4fc91b322b628c2631e417b55c55cb5
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-12-18 09:49:28 +0900

    暫定で時間平均をプロットする機能を付けた

--- report_01/8-2c/PlotCSV.py
+++ report_01/8-2c/PlotCSV.py
@@ -21,13 +21,21 @@ plt.savefig("./PlotCSV.png")
 time_average = []
 sum_t = 0
 for num in col_y:
-    sum_t == num
+    sum_t += num
     time_average.append(sum_t)
 
-print(f"len(time_average) = {len(time_average)}, len(col_y) = {len(col_y)} ... これらの値が同値であることを期待している")
+print(f"type(time_average) = {type(time_average)}, type(col_x) = {type(col_x)}")
+print(f"len(time_average) = {len(time_average)}, len(col_y) = {len(col_x)} ... これらの値が同値であることを期待している")
 
-for t in col_y:
-    if t == 0:
+times = col_x.tolist()
+
+for i in range(len(times)):
+    if times[i] == 0:
         continue
-    else:
-        pass
+    time_average[i] /= times[i]
+
+plt.figure()
+plt.plot(times, time_average)
+plt.xlabel("t")
+plt.ylabel("time_average")
+plt.savefig("./PlotCSV2.png")
