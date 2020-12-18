e03593af03bafebd7bd35d8c82fc75668b9fc0f2
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-12-18 10:52:43 +0900

    回帰直線を引いた

--- report_01/8-2c/PlotCSV.py
+++ report_01/8-2c/PlotCSV.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 Output = pd.read_csv("./a_output.csv")
 
@@ -13,6 +14,19 @@ col_x = Output["t"]
 col_y = Output["n(t)"]
 
 plt.plot(col_x, col_y)
+
+# n(t)に対して回帰直線を引く
+## x, y軸を成形
+x = col_x.to_numpy()
+y = col_y.to_numpy()
+x = x.reshape(-1,1)
+y = y.reshape(-1,1)
+## 線形回帰
+mod = LinearRegression()
+mod_lin = mod.fit(x, y)
+y_lin_fit = mod_lin.predict(x)
+plt.plot(x, y_lin_fit, color = 'r', linewidth=1.5)
+
 plt.xlabel("t")
 plt.ylabel("n(t)")
 plt.savefig("./PlotCSV.png")
