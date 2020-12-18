191500e887413e885608e6a25ee4e6bd4f4e8a58
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-12-18 15:50:25 +0900

    不要部を削除

--- report_01/8-3a/PlotCSV.py
+++ report_01/8-3a/PlotCSV.py
@@ -52,27 +52,4 @@ plt.xlabel("t")
 plt.ylabel("n(t)")
 plt.savefig("./PlotCSV.png")
 
-
-# time_average = []
-# sum_t = 0
-# for num in col_y:
-#     sum_t += num
-#     time_average.append(sum_t)
-
-# print(f"type(time_average) = {type(time_average)}, type(col_x) = {type(col_x)}")
-# print(f"len(time_average) = {len(time_average)}, len(col_y) = {len(col_x)} ... これらの値が同値であることを期待している")
-
-# times = col_x.tolist()
-
-# for i in range(len(times)):
-#     if times[i] == 0:
-#         continue
-#     time_average[i] /= times[i]
-
-# plt.figure()
-# plt.plot(times, time_average)
-# plt.xlabel("t")
-# plt.ylabel("time_average")
-# plt.savefig("./PlotCSV2.png")
-
 plot_TimeAverage(col_x.tolist(), col_y.tolist(), "./PlotCSV2.png")
