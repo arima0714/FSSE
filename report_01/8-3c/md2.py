37e500c3e2a1baf6c4f823ebea3050f1078d3ed9
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-12-19 12:14:37 +0900

    時間の指定方法を変更した

--- report_01/8-3c/md2.py
+++ report_01/8-3c/md2.py
@@ -316,12 +316,14 @@ while True:
     ncum = ncum + 1
     show_output()
 
-    if ( t == 1.0):
+    tmp_t = t
+    tmp_t = int(tmp_t * 100)/100
+    if ( tmp_t == 1.0):
         for i in range(N):
             vx[i] = -1 * vx[i]
             vy[i] = -1 * vy[i]
 
-    if ( t > 2.0):
+    if ( tmp_t > 2.0):
         break
 
     plt.figure()
