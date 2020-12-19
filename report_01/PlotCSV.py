96956ea047188881d2887b3f74e6c1c37467b648
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-12-19 17:05:20 +0900

    E/mのプロットを行うようにした

--- report_01/PlotCSV.py
+++ report_01/PlotCSV.py
@@ -15,3 +15,19 @@ col_y = EulerRichardsonOutput[Y]
 
 plt.plot(col_x, col_y)
 plt.savefig("./PlotCSV.png")
+
+# E/m(=全エネルギー)のプロットを行う
+E_per_m = EulerRichardsonOutput[columns[2]]
+
+time = []
+time_sum = 0
+dt = 0.01
+for i in range(len(E_per_m)):
+    time.append(time_sum)
+    time_sum += dt
+
+plt.figure()
+plt.plot(time, E_per_m)
+plt.savefig("./Plot_E_per_m.png")
+
+print(f"len(time) = {len(time)}, len(E_per_m) = {len(E_per_m)}")
